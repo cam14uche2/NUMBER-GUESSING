0cb18ab540f10d6acdeaaf8e4a2f4eafcaaf60e3
cam14uche camillus <>
2022-10-20 20:20:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// prompt user for username and display it
+
+const username = prompt("What is your name?");
+
+// welcome user using username
+
+console.log(`welcome ${username}`);
+
+alert("you have 8 guesses. Guess 1-2");
+
+var answer =3;
+var guess = prompt("what's your guess");
+
+for (i=0; i<2;i++){
+  if(answer == guess){
+    alert("You guessed correctly");
+    break;
+  }
+  else{
+    guess = prompt("please try again");
+  }
+}
